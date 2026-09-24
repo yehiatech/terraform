c9d5cbaeab0edd5a15418d9246f8475c3fbeb4d5
yehiatech <yehia@fenec.tech>
2026-09-24 15:19:35 +0300

    tera-2: chore (vpc-module): created a vpc  for  the ec2

--- main.tf
+++ main.tf
@@ -11,8 +11,11 @@ data "aws_ami" "ubuntu" { //data goes and fetch the  recent ami version
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.server_type
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.server_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
 
   tags = {
     Environment = var.environment
@@ -20,4 +23,17 @@ resource "aws_instance" "app_server" {
   }
 
 
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "dev-server-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
 }
